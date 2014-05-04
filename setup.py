fd38b69d415c13c7786282783820e5f67b24833e
jeff <jeff@fallever.com>
2014-05-04 10:13:48 +0800

    增加setup文件

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from goldencage import VERSION
+
+url="https://github.com/jeffkit/goldencage"
+
+long_description="virtual coin & task management for mobile app (specially for china)"
+
+setup(name="goldencage",
+      version=VERSION,
+      description=long_description,
+      maintainer="jeff kit",
+      maintainer_email="bbmyth@gmail.com",
+      url = url,
+      long_description=long_description,
+      packages=find_packages('.'),
+     )
+
+
