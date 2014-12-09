3a53f9fe8df41dead0fce8ff817db829a782eb76
msheng <=>
2014-12-09 23:28:35 +0800

    don't repeat yourself

--- goldencage/views.py
+++ goldencage/views.py
@@ -395,27 +395,45 @@ def wechat_pay_gen_package(request):
     return rsp(data)
 
 
-def convert_params_to_str_in_order(params):
-    param_list = sorted(params.iteritems(), key=lambda d: d[0])
-    log.debug(u'param_list = %s' % param_list)
-    tmp_str = u''
-    for val in param_list:
-        if tmp_str:
-            tmp_str = tmp_str + '&%s=%s' % (val[0], val[1])
-        else:
-            tmp_str = '%s=%s' % (val[0], val[1])
-    return tmp_str.encode('utf-8')
+# def convert_params_to_str_in_order(params):
+#     param_list = sorted(params.iteritems(), key=lambda d: d[0])
+#     log.debug(u'param_list = %s' % param_list)
+#     tmp_str = u''
+#     for val in param_list:
+#         if tmp_str:
+#             tmp_str = tmp_str + '&%s=%s' % (val[0], val[1])
+#         else:
+#             tmp_str = '%s=%s' % (val[0], val[1])
+#     return tmp_str.encode('utf-8')
 
 
-def convert_params_to_str_in_order_urlcode(params):
+# def convert_params_to_str_in_order_urlcode(params):
+#     param_list = sorted(params.iteritems(), key=lambda d: d[0])
+#     log.debug('param_list = %s' % param_list)
+#     tmp_str = u''
+#     for val in param_list:
+#         vall = u'%s' % val[1]
+#         vall = vall.encode('utf-8')
+#         log.debug('vall = %s' % vall)
+#         vall = urllib.quote(vall)
+#         if tmp_str:
+#             append_str = u'&%s=%s' % (val[0], vall)
+#             tmp_str = tmp_str + append_str
+#         else:
+#             tmp_str = u'%s=%s' % (val[0], vall)
+#     return tmp_str.encode('utf-8')
+
+
+def convert_params_to_str_in_order(params, urlencode=False):
     param_list = sorted(params.iteritems(), key=lambda d: d[0])
     log.debug('param_list = %s' % param_list)
     tmp_str = u''
     for val in param_list:
         vall = u'%s' % val[1]
-        vall = vall.encode('utf-8')
+        if urlencode:
+            vall = vall.encode('utf-8')
+            vall = urllib.quote(vall)
         log.debug('vall = %s' % vall)
-        vall = urllib.quote(vall)
         if tmp_str:
             append_str = u'&%s=%s' % (val[0], vall)
             tmp_str = tmp_str + append_str
@@ -452,7 +470,7 @@ def _wechatpay_gen_package(
     md5.update(stringSignTemp)
     sign_str = md5.hexdigest().upper()
     log.debug(u'sign = %s' % sign_str)
-    string1 = convert_params_to_str_in_order_urlcode(package)
+    string1 = convert_params_to_str_in_order(package, urlencode=True)
     package = string1 + '&sign=%s' % sign_str
     return package
 
